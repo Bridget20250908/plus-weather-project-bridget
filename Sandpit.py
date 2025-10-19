824e99fefee17b7b4d5ea811a99305f841cf1bc6
Bridget <bridget.tang@yahoo.com>
2025-10-19 20:09:42 +1000

    5 steps are done

---
+++ Sandpit.py
@@ -0,0 +1,42 @@
+def generate_summary(weather_data):
+    """Outputs a summary for the given weather data.
+
+    Args:
+        weather_data: A list of lists, where each sublist represents a day of weather data.
+    Returns:
+        A string containing the summary information:
+
+        5 Day Overview
+    The lowest temperature will be 9.4°C, and will occur on Friday 02 July 2021.
+    The highest temperature will be 20.0°C, and will occur on Saturday 03 July 2021.
+    The average low this week is 12.2°C.
+    The average high this week is 17.8°C.
+    """
+    low_temp_list = []
+    for i in weather_data:
+        low_temp = i[1]
+        low_temp_list.append(low_temp)
+    lowest_temp = min(low_temp_list)
+    lowest_temp_index = low_temp_list.index(lowest_temp)
+    print(lowest_temp, lowest_temp_index)
+
+    high_temp_list = []
+    for i in weather_data:
+        high_temp = i[2]
+        high_temp_list.append(high_temp)
+    highest_temp = max(high_temp_list)
+    highest_temp_index = high_temp_list.index(highest_temp)
+    print(highest_temp, highest_temp_index)
+
+    #     min_index = weather_data.index(lowest_temp)
+    #     lowest_date = weather_data[min_index[0]]
+    # return (lowest_date, lowest_temp)
+weather_data = [["2020-06-19T07:00:00+08:00", -7, 10],
+                ["2020-06-20T07:00:00+08:00", -51, 67],
+                ["2020-06-21T07:00:00+08:00", 58, 72],
+                ["2020-06-22T07:00:00+08:00", 59, 71],
+                ["2020-06-23T07:00:00+08:00", -52, 71],
+                ["2020-06-24T07:00:00+08:00", 52, 67],
+                ["2020-06-25T07:00:00+08:00", -48, 66],
+                ["2020-06-26T07:00:00+08:00", 53, 66]]
+print(generate_summary(weather_data))
--- weather.py
+++ weather.py
@@ -7,12 +7,17 @@ DEGREE_SYMBOL = u"\N{DEGREE SIGN}C"
 def format_temperature(temp):
     """Takes a temperature and returns it in string format with the degrees
         and Celcius symbols.
-
     Args:
         temp: A string representing a temperature.
     Returns:
         A string contain the temperature and "degrees Celcius."
     """
+#   with open(".tests/data/example.csv") as file:
+#       reader = csv.reader(file)
+#       next(reader)
+#       for row in reader:
+#           temp_min = row[1]
+#           temp_max = row[2]
     return f"{temp}{DEGREE_SYMBOL}"
 
 
@@ -24,7 +29,11 @@ def convert_date(iso_string):
     Returns:
         A date formatted like: Weekday Date Month Year e.g. Tuesday 06 July 2021
     """
-    pass
+
+    date_object = datetime.fromisoformat(iso_string)
+    date_string = date_object.strftime("%A %d %B %Y")
+
+    return date_string
 
 
 def convert_f_to_c(temp_in_fahrenheit):
@@ -35,7 +44,9 @@ def convert_f_to_c(temp_in_fahrenheit):
     Returns:
         A float representing a temperature in degrees Celcius, rounded to 1 decimal place.
     """
-    pass
+    temp_in_celcisu = (float(temp_in_fahrenheit) - 32) * 5 / 9
+
+    return round(temp_in_celcisu, 1)
 
 
 def calculate_mean(weather_data):
@@ -46,7 +57,14 @@ def calculate_mean(weather_data):
     Returns:
         A float representing the mean value.
     """
-    pass
+    weather_data_sum = 0
+    for i in weather_data:
+        weather_data_sum = weather_data_sum+float(i)
+    if len(weather_data) != 0:
+        mean = weather_data_sum / len(weather_data)
+        return mean
+    else:
+        return f"No Weather Data Found"
 
 
 def load_data_from_csv(csv_file):
@@ -57,7 +75,16 @@ def load_data_from_csv(csv_file):
     Returns:
         A list of lists, where each sublist is a (non-empty) line in the csv file.
     """
-    pass
+    with open(csv_file) as file:
+        reader = csv.reader(file)
+        next(reader)
+        count = 0
+    for row in reader:
+        count = count+1
+        return count
+    for i in count:
+        list[i] = row[i]
+        return list
 
 
 def find_min(weather_data):
@@ -68,7 +95,17 @@ def find_min(weather_data):
     Returns:
         The minimum value and it's position in the list. (In case of multiple matches, return the index of the *last* example in the list.)
     """
-    pass
+
+    #  last_index = max(i for i, x in enumerate(my_list) if x == search_value)
+    if not weather_data:
+        return ()
+    for i in weather_data:
+
+        min_value = min(weather_data)
+        min_last_index = max(i for i, x in enumerate(
+            weather_data) if x == min_value)
+        # min_index = weather_data.index(min_value)
+    return (float(min_value), min_last_index)
 
 
 def find_max(weather_data):
@@ -79,7 +116,15 @@ def find_max(weather_data):
     Returns:
         The maximum value and it's position in the list. (In case of multiple matches, return the index of the *last* example in the list.)
     """
-    pass
+    if not weather_data:
+        return ()
+    for i in weather_data:
+
+        max_value = max(weather_data)
+        # max_index = weather_data.index(max_value)
+        max_last_index = max(i for i, x in enumerate(
+            weather_data) if x == max_value)
+    return (float(max_value), max_last_index)
 
 
 def generate_summary(weather_data):
@@ -88,9 +133,32 @@ def generate_summary(weather_data):
     Args:
         weather_data: A list of lists, where each sublist represents a day of weather data.
     Returns:
-        A string containing the summary information.
+        A string containing the summary information:
+
+        5 Day Overview
+    The lowest temperature will be 9.4°C, and will occur on Friday 02 July 2021.
+    The highest temperature will be 20.0°C, and will occur on Saturday 03 July 2021.
+    The average low this week is 12.2°C.
+    The average high this week is 17.8°C.
     """
-    pass
+    for i in weather_data:
+        lowest_temp = min(weather_data)
+        min_index = weather_data.index(lowest_temp)
+        lowest_date = convert_date(weather_data[min_index[0]])
+
+        highest_temp = max(weather_data)
+
+        lowest_temp_date = weather_data.index(lowest_temp)
+
+        highest_temp_date = weather_data.index(highest_temp)
+
+        average_low = calculate_mean(weather_data)
+        average_high = calculate_mean(weather_data)
+
+        multi_string = """
+"""
+
+        return (f"5 Day Overview\n The lowest temperatur will be {lowest_temp}, {DEGREE_SYMBOL}, and will occur on {lowest_temp_date}.\n The highest temperature will be {highest_temp}, {DEGREE_SYMBOL}, and will occur on {highest_temp_date}.\n The average low this week is {average_low}, {DEGREE_SYMBOL}.\n The average high this week is {average_high}, {DEGREE_SYMBOL}\n")
 
 
 def generate_daily_summary(weather_data):
@@ -101,4 +169,6 @@ def generate_daily_summary(weather_data):
     Returns:
         A string containing the summary information.
     """
-    pass
+
+
+pass
